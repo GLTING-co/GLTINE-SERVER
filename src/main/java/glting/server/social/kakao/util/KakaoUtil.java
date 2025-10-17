f878df5c76d28facc4afb934d63a29ce0d8283d6
euics <pjh575157@gmail.com>
2025-10-17 18:56:51 +0900

    chore(kakao): 카카오 API URL 상수 유틸 클래스 추가
    - KAKAO_AUTH_BASE_URL 및 KAKAO_API_BASE_URL 상수 정의
    - 도메인 내 WebClient 구성 시 재사용 가능하도록 분리

---
+++ src/main/java/glting/server/social/kakao/util/KakaoUtil.java
@@ -0,0 +1,6 @@
+package glting.server.social.kakao.util;
+
+public class KakaoUtil {
+    public static final String KAKAO_AUTH_BASE_URL = "https://kauth.kakao.com";
+    public static final String KAKAO_API_BASE_URL = "https://kapi.kakao.com";
+}
