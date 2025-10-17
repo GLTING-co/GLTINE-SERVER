5dd1e40de46d1379b4d40d8708407bb3d056928b
euics <pjh575157@gmail.com>
2025-10-17 18:57:56 +0900

    feat(config): Swagger(OpenAPI 3) 설정 추가
    - OpenAPI Bean 구성 및 서버 URL yml에서 주입
    - JWT 인증 스키마(SecurityScheme) 추가
    - API 설명, 버전, 보안 스키마 등 기본 메타정보 설정
    - 명세서에 공통 응답 규칙 및 담당자 안내 문구 추가

---
+++ src/main/java/glting/server/config/SwaggerConfiguration.java
@@ -0,0 +1,53 @@
+package glting.server.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfiguration {
+    @Value("${swagger.server-url}")
+    private String serverUrl;
+
+    @Bean
+    public OpenAPI openAPI() {
+        String jwt = "JWT";
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList(jwt);
+        Components components = new Components().addSecuritySchemes(jwt, new SecurityScheme()
+                .name(jwt)
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT")
+        );
+
+        Server server = new Server();
+        server.setUrl(serverUrl);
+
+        return new OpenAPI()
+                .components(new Components())
+                .info(apiInfo())
+                .addSecurityItem(securityRequirement)
+                .components(components)
+                .addServersItem(server);
+    }
+
+    private Info apiInfo() {
+        return new Info()
+                .title("GLTING")
+                .description("""
+                        1. 프론트에서 분기 잡을 수 있게 CUSTOM CODE 값을 같이 반환합니다.
+                        2. EXCEPTION 시 **httpStatus, message, code**의 형식이 아닌 **다른 형식**으로 날라갈 경우 *담당자*에게 말씀해주세요.
+                        3. CODE 값에 **"UNAUTHORIZED_EXCEPTION_예외코드 설정하세요."**가 반환될 경우 *담당자*에게 말씀해주세요.
+                        4. 필요한 API 혹은 데이터가 있을 경우 말씀해주세요.
+                        5. 각각의 API Description에 빠져있는 EXCEPTION이 있을 경우 말씀해주세요.
+                        6. 객체 반환값의 정보는 명세서 아래 객체 정보에서 확인할 수 있습니다.
+                        """)
+                .version("1.0.0");
+    }
+}
